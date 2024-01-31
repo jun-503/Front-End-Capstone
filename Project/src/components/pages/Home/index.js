4c8fe64423bd459e5a78b7de8d2e040dc384615b
Junaid <junaidzia697@gmail.com>
2024-01-31 17:55:13 +0500

    Final Project

---
+++ Project/src/components/pages/Home/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecial';
+import Testimonials from './Testimonials';
+import OurStory from './OurStory';
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <WeekSpecials />
+            <Testimonials />
+            <OurStory />
+        </>
+    );
+};
+
+export default Home;
